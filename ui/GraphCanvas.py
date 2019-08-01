f75c0ebde4af8c88cbdd30509c4a0894063e3bc7
Anting Zhou <572480656@qq.com>
2019-08-01 20:33:59 +0800

    19.8.1 move/zoom graph, add route
    Done move and zoom graph.
    Done add route but not test it.

--- ui/GraphCanvas.py
+++ ui/GraphCanvas.py
@@ -1,5 +1,7 @@
 import osmnx as ox
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+from matplotlib.figure import Figure
+import pickle
 
 def TouchPairID(touch1, touch2):
 	if touch1.uid < touch2.uid:
@@ -9,13 +11,33 @@ def TouchPairID(touch1, touch2):
 
 class GraphCanvas(FigureCanvasKivyAgg):
 	def __init__(self):
+		super().__init__(Figure())
 		self.touches = {} # manage multitouch
 		self.touchCenter = [0, 0]
 		self.touchDistMap = {}
 		self.zoomCoeff = 1
-		graph = ox.graph_from_place('Modena, Italy', infrastructure='way["power"~"line"]')
-		fig, ax = ox.plot_graph(graph, show=False, close=True)
-		super().__init__(fig)
+		try:
+			with open("graph.data", "rb") as f:
+				self.G = pickle.load(f)
+		except IOError:
+			self.G = ox.graph_from_place('Modena, Italy', infrastructure='way["power"~"line"]')
+			with open("graph.data", "wb") as f:
+				pickle.dump(self.G, f)
+		self.figure, self.ax = ox.plot_graph(self.G, show=False, close=True)
+		self.routeColor = 'red'
+		self.routes = []
+
+	def addRoute(self, route):
+		"""
+		"""
+		self.routes.append(route)
+		self.figure, self.ax = ox.plot_graph_route(self.G, self.routes, show=False, close=True, route_color=self.routeColor)
+		self.draw_idle()
+
+	def clearRoutes(self):
+		self.routes = []
+		self.figure, self.ax = ox.plot_graph(self.G, show=False, close=True)
+		self.draw_idle()
 
 	def updateDistMap(self, touch):
 		for t in self.touches.values():
@@ -46,17 +68,28 @@ class GraphCanvas(FigureCanvasKivyAgg):
 			if t.uid != touch.uid:
 				self.touchDistMap.pop(TouchPairID(touch,t), None)
 
-	def zoomInGraph(self, movement):
-		print("GraphCanvas: zoom in: {}".format(movement))
-		
-
-	def zoomOutGraph(self, movement):
-		print("GraphCanvas: zoom out: {}".format(movement))
-		
+	def zoomGraph(self, scale):
+		"""
+		zoom graph
+		todo: init self xlim for graph
+		"""
+		print("GraphCanvas: zoom in: {}".format(scale))
+		scalePer = scale/self.width
+		xmin, xmax = self.ax.get_xlim()
+		self.ax.set(xlim=((xmin+xmax)/2-(xmax-xmin)*(1+scalePer)/2, (xmin+xmax)/2+(xmax-xmin)*(1+scalePer)/2))
+		self.draw_idle()
 
 	def moveGraph(self, x, y):
 		print("GraphCanvas: move: {}, {}".format(x, y))
-		
+		xPer = x/self.width
+		xmin, xmax = self.ax.get_xlim()
+		xmove = (xmax-xmin)*xPer
+		self.ax.set(xlim=(xmin-xmove, xmax-xmove))
+		yPer = y/self.height
+		ymin, ymax = self.ax.get_ylim()
+		ymove = (ymax-ymin)*yPer
+		self.ax.set(ylim=(ymin-ymove, ymax-ymove))
+		self.draw_idle()
 
 	def on_touch_down(self, touch):
 		self.touches[touch.uid] = touch
@@ -71,12 +104,9 @@ class GraphCanvas(FigureCanvasKivyAgg):
 		lastDist = self.calAvgDist(touch)
 		self.updateDistMap(touch)
 		nowDist = self.calAvgDist(touch)
-		if lastDist < nowDist:
-			self.zoomOut(nowDist - lastDist)
-		if nowDist < lastDist:
-			self.zoomIn(lastDist - nowDist)
-			
-		if self.collide_point(*touch.pos):
+		if len(self.touches) > 1:
+			self.zoomGraph(lastDist - nowDist)	
+		elif self.collide_point(*touch.pos):
 			px, py = self.touchCenter
 			self.updateTouchCenter()
 			x, y = self.touchCenter
